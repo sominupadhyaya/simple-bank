3ce8fce8cdf40dae9f25991f696ce0e9b6862281
Somin <sominupadhyaya@gmail.com>
2022-07-13 15:29:43 +0545

    Complete project

--- src/App.tsx
+++ src/App.tsx
@@ -14,25 +14,31 @@ const  App = () => {
   const [deposits , setDeposits] = useState(0)
   return (
     <div className="App">
+      
       <label htmlFor="deposit">Enter the amount you want to deposit : </label>
+      
       <input id="deposit" type="text" onChange={(e) =>{
       if (!isNaN(parseInt(e.target.value))) {
           setDeposits(parseInt(e.target.value))
       }
       }}/>
-  
-      <br />
-      <br />
+
+      <br /> <br />
       
       <label htmlFor="withdraw">Enter the amount you want to withdraw : </label>
+      
       <input type="text" id="withdraw" onChange={(e) => {
       if(!isNaN(parseInt(e.target.value))){
          setWithdraws(parseInt((e.target.value))) 
       }
       }}/>
+
     <h1>{amount}</h1>
+    
     <button type="submit" onClick={() => deposit(deposits)}>Deposit</button>
+    
     <button type="submit" onClick={() => withdraw(withdraws)}>Withdraw</button> 
+    
     <button onClick={() => bankrupt()}>Bankrupt</button>
     </div>
   );
