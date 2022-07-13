5c2788367f417be1d92510b2dc93764ae7f70343
Somin <sominupadhyaya@gmail.com>
2022-07-13 11:52:41 +0545

    redux implementation completed

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from "redux";
+import { legacy_createStore as createStore, applyMiddleware } from "redux";
 import reducers from "./reducers/index";
 
 import thunk from "redux-thunk"
